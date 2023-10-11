545bd1f299ba2502c0603c9bf1b46eedf3c9fce3
patricksmile123 <146002914+patricksmile123@users.noreply.github.com>
2023-10-11 21:25:12 +0100

    Add files via upload

---
+++ Caesar Cypher Text.py
@@ -0,0 +1,15 @@
+plainText = open("encrypted.txt", 'r')
+distance = int(input("Please input the distance of the Caesar Cypher "))
+code = " "
+ordvalue = " "
+for ch in plainText:
+    len(ch) == 1
+    ordvalue = ord(ch)
+    cipherValue = ordvalue + distance
+    if cipherValue > ord('z'):
+        cipherValue = ord('a') + distance - (ord)('z') - ordvalue+1
+    code += chr(cipherValue)
+print(code)
+file = open("encrypted.txt", 'w')
+file.write(code)
+file.close()
